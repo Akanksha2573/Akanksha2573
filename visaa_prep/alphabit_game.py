4dd82334ebd0bbdb01b5709961e375e4197f82b7
Janyavula Akanksha <janyavulaakanksha@gmail.com>
2024-11-19 18:27:46 +0530

    Create alphabit_game.py

---
+++ visaa_prep/alphabit_game.py
@@ -0,0 +1,11 @@
+s = input().strip()
+vowels = "aeiouAEIOU"
+vowel_count = 0
+consonant_count = 0
+for char in s:
+    if char.isalpha(): 
+        if char in vowels:
+            vowel_count += 1
+        else:
+            consonant_count += 1
+print(f"{vowel_count},{consonant_count}")
